ed2b6f31b766f1ff8d5cde82fba24039045aea4a
buildingsite <worker1.buildingsite@gmail.com>
2020-06-12 15:49:10 +0200

    changes

---
+++ lib/app.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import './screens/home.dart';
+
+class App extends StatelessWidget{
+  Widget build(BuildContext context){
+    return MaterialApp(
+      home: Home(),
+    );
+  }
+}
